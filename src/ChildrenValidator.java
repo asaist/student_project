8c0290df619f9bf563283f58d464ac3d8057bc4c
kosinevskiy <kosakvova@gmail.com>
2022-05-12 23:03:46 +0300

    classes and objects

---
+++ src/ChildrenValidator.java
@@ -0,0 +1,5 @@
+public class ChildrenValidator {
+    static AnswerChildren checkChildren(StudentOrder so){
+        return new AnswerChildren();
+    }
+}
--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -3,22 +3,53 @@ public class StudentOrderValidator {
         checkAll();
     }
     static void checkAll(){
-        checkCityRegister();
-        checkWedding();
-        checkChildren();
-        checkStudent();
+        while (true) {
+            StudentOrder so = readStudentOrder();
+            System.out.println("Start");
+            if (so == null){
+                break;
+            }
+            System.out.println("Finish");
+            AnswerCityRegister cityAnswer = checkCityRegister(so);
+            if (!cityAnswer.success){
+                break;
+            }
+
+            AnswerWedding answerWedding = checkWedding(so);
+            AnswerChildren answerChildren = checkChildren(so);
+            AnswerStudent answerStudent = checkStudent(so);
+            sendMail(so);
+        }
+        System.out.println("Finish 2");
     }
 
-    static void checkCityRegister(){
-        System.out.println("CityRegister is running");
+    static StudentOrder readStudentOrder(){
+        StudentOrder so = new StudentOrder();
+        return so;
     }
-    static void checkWedding(){
-        System.out.println("Wedding is running");
+    static AnswerCityRegister checkCityRegister(StudentOrder so){
+        CityRegisterValidator crv1 = new CityRegisterValidator();
+        crv1.hostName = "Host1";
+        crv1.login ="login1";
+        crv1.password= "password1";
+        CityRegisterValidator crv2 = new CityRegisterValidator();
+        crv2.hostName = "Host2";
+        crv2.login ="login2";
+        crv2.password= "password2";
+        AnswerCityRegister ans1 = crv1.checkCityRegister(so);
+        AnswerCityRegister ans2 = crv2.checkCityRegister(so);
+        return ans1;
     }
-    static void checkChildren(){
-        System.out.println("ChildrenCheck is running");
+    static AnswerWedding checkWedding(StudentOrder so){
+        return WeddingValidator.checkWedding(so);
     }
-    static void checkStudent(){
-        System.out.println("CheckStudent is running");
+    static AnswerChildren checkChildren(StudentOrder so){
+        return ChildrenValidator.checkChildren(so);
+    }
+    static AnswerStudent checkStudent(StudentOrder so){
+        return StudentValidator.checkStudent(so);
+    }
+    static void sendMail(StudentOrder so){
+        System.out.println("Mail is sending");
     }
 }
