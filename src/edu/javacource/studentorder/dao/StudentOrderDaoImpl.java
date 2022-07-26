6e68f648334dbef28ce19806e1bfadc2b1f1cb30
kosinevskiy <kosakvova@gmail.com>
2022-07-26 17:16:28 +0400

    refactor-

--- src/edu/javacource/studentorder/dao/StudentOrderDaoImpl.java
+++ src/edu/javacource/studentorder/dao/StudentOrderDaoImpl.java
@@ -11,7 +11,7 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
 
     private static final String INSERT_ORDER =
             "INSERT INTO public.jc_student_order(" +
-                    "student_order_status, student_order_date, h_sur_name, h_given_name, h_patronymic," +
+                    " student_order_status, student_order_date, h_sur_name, h_given_name, h_patronymic," +
                     " h_date_of_birth, h_passport_seria, h_passport_number, h_passport_date, h_passport_office_id, h_post_index," +
                     " h_street_code, h_building, h_extension, h_apartment, w_sur_name, w_given_name, w_patronymic, w_date_of_birth," +
                     " w_passport_seria, w_passport_number, w_passport_date, w_passport_office_id, w_post_index, w_street_code, w_building," +
@@ -21,6 +21,16 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
                     "        ?, ?, ?, ?, ?, ?, ?, ?," +
                     "        ?, ?, ?, ?, ?, ?, ?," +
                     "        ?, ?, ?, ?, ?); ";
+    private static final String INSERT_CHILD =
+            "INSERT INTO jc_student_child(" +
+                    " student_order_id, c_sur_name, c_given_name," +
+                    " c_patronymic, с_date_of_birth, c_certificate_number, c_issue_date," +
+                    " c_register_office_id, c_post_index, c_street_code, c_building," +
+                    " c_extension, c_apartment)" +
+                    "VALUES (?, ?, ?, " +
+                    "?, ?, ?, ?," +
+                    "?, ?, ?, ?," +
+                    " ?, ?); ";
 
     //TODO refactoring - make one method
     private Connection getConnection() throws SQLException {
@@ -58,27 +68,52 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
             }
             gkRs.close();
 
+            saveChildren(con,so,result);
+
         } catch (SQLException ex) {
             throw new DaoException(ex);
         }
         return result;
     }
-
+    private void saveChildren(Connection con, StudentOrder so, Long soId) throws SQLException {
+        try(PreparedStatement stmt = con.prepareStatement(INSERT_CHILD)) {
+            for (Child child : so.getChildren()) {
+                stmt.setLong(1,soId);
+                setParamsForChild(stmt,child);
+                stmt.executeUpdate();
+            }
+        }catch (SQLException ex){
+            ex.printStackTrace();
+            ex.getErrorCode();
+        }
+    }
     private void setParamsForAdult(PreparedStatement stmt, int start, Adult adult) throws SQLException {
-        stmt.setString(start, adult.getSurName());
-        stmt.setString(start +1, adult.getGivenName());
-        stmt.setString(start +2, adult.getPatronymic());
-        stmt.setDate(start +3, Date.valueOf(adult.getDateOfBirth()));
+        setParamsForPerson(stmt, start, adult);
         stmt.setString(start +4, adult.getPassportSeria());
         stmt.setString(start +5, adult.getPassportNumber());
         stmt.setDate(start +6, Date.valueOf(adult.getIssueDate()));
         stmt.setLong(start +7, adult.getIssueDepartment().getOfficeId());
-        Address h_address = adult.getAddress();
-        stmt.setString(start +8,h_address.getPostCode());
-        stmt.setLong(start +9,h_address.getStreet().getStreetCode());
-        stmt.setString(start +10,h_address.getBuilding());
-        stmt.setString(start +11,h_address.getExtension());
-        stmt.setString(start +12,h_address.getApartment());
+        setParamsForAddress(stmt, start + 8, adult);
     }
-
+    private void setParamsForChild(PreparedStatement stmt, Child child) throws SQLException{
+        setParamsForPerson(stmt,2,child);
+        stmt.setString(6,child.getCertificateNumber());
+        stmt.setDate(7,java.sql.Date.valueOf(child.getIssueDate()));
+        stmt.setLong(8,child.getIssueDepartment().getOfficeId());
+        setParamsForAddress(stmt,9,child);
+    }
+    private void setParamsForPerson(PreparedStatement stmt, int start, Person person) throws SQLException {
+        stmt.setString(start, person.getSurName());
+        stmt.setString(start +1, person.getGivenName());
+        stmt.setString(start +2, person.getPatronymic());
+        stmt.setDate(start +3, Date.valueOf(person.getDateOfBirth()));
+    }
+    private void setParamsForAddress(PreparedStatement stmt, int start, Person person) throws SQLException {
+        Address h_address = person.getAddress();
+        stmt.setString(start ,h_address.getPostCode());
+        stmt.setLong(start +1,h_address.getStreet().getStreetCode());
+        stmt.setString(start +2,h_address.getBuilding());
+        stmt.setString(start +3,h_address.getExtension());
+        stmt.setString(start +4,h_address.getApartment());
+    }
 }
--- student_project_structure.sql
+++ student_project_structure.sql
@@ -1,44 +1,46 @@
-drop table if exists jc_student_order;
-drop table if exists jc_student_child;
-drop table if exists jc_passport_office;
-drop table if exists jc_register_office;
-drop table if exists jc_country_struct;
-drop table if exists jc_street;
+DROP TABLE IF EXISTS jc_student_child;
+DROP TABLE IF EXISTS jc_student_order;
+DROP TABLE IF EXISTS jc_passport_office;
+DROP TABLE IF EXISTS jc_register_office;
+DROP TABLE IF EXISTS jc_country_struct;
+DROP TABLE IF EXISTS jc_street;
 
-create table  jc_street
+CREATE TABLE jc_street
 (
-	street_code integer not null,
-	street_name varchar(300),
-	primary key (street_code)
+    street_code integer not null,
+    street_name varchar(300),
+    PRIMARY KEY (street_code)
 );
 
-create table jc_country_struct
+CREATE TABLE jc_country_struct
 (
-	area_id char(12) not null,
-	area_name varchar(200),
-	primary key (area_id)
+    area_id char(12) not null,
+    area_name varchar(200),
+    PRIMARY KEY (area_id)
 );
 
-create table jc_passport_office
+CREATE TABLE jc_passport_office
 (
-	p_office_id integer not null,
-	p_office_area_id char(12),
-	p_office_name varchar(200),
-	primary key (p_office_id),
-	foreign key (p_office_area_id) references jc_country_struct(area_id) on delete restrict
-);
-create table jc_register_office
-(
-	r_office_id integer not null,
-	r_office_area_id char(12),
-	r_office_name varchar(200),
-	primary key (r_office_id),
-	foreign key (r_office_area_id) references jc_country_struct(area_id) on delete restrict
+    p_office_id integer not null,
+    p_office_area_id char(12) not null,
+    p_office_name varchar(200),
+    PRIMARY KEY (p_office_id),
+    FOREIGN KEY (p_office_area_id) REFERENCES jc_country_struct(area_id) ON DELETE RESTRICT
 );
 
-create table jc_student_order
+CREATE TABLE jc_register_office
 (
-    student_order_id serial,
+    r_office_id integer not null,
+    r_office_area_id char(12) not null,
+    r_office_name varchar(200),
+    PRIMARY KEY (r_office_id),
+    FOREIGN KEY (r_office_area_id) REFERENCES jc_country_struct(area_id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE jc_student_order
+(
+    student_order_id SERIAL,
     student_order_status int not null,
     student_order_date timestamp not null,
     h_sur_name varchar(100) not null,
@@ -54,48 +56,45 @@ create table jc_student_order
     h_building varchar(10) not null,
     h_extension varchar(10),
     h_apartment varchar(10),
-
-        w_sur_name varchar(100) not null,
-        w_given_name varchar(100) not null,
-        w_patronymic varchar(100) not null,
-        w_date_of_birth date not null,
-        w_passport_seria varchar(10) not null,
-        w_passport_number varchar(10) not null,
-        w_passport_date date not null,
-        w_passport_office_id integer not null,
-        w_post_index varchar(10),
-        w_street_code integer not null,
-        w_building varchar(10) not null,
-        w_extension varchar(10),
-        w_apartment varchar(10),
-
-        certificate_id varchar(10) not null,
-        register_office_id integer not null,
-        marriage_date date not null,
-
-        primary key (student_order_id),
-        foreign key (h_street_code) references jc_street(street_code) on delete restrict,
-        foreign key (w_street_code) references jc_street(street_code) on delete restrict,
-        foreign key (register_office_id) references jc_register_office(r_office_id) on delete restrict
+    w_sur_name varchar(100) not null,
+    w_given_name varchar(100) not null,
+    w_patronymic varchar(100) not null,
+    w_date_of_birth date not null,
+    w_passport_seria varchar(10) not null,
+    w_passport_number varchar(10) not null,
+    w_passport_date date not null,
+    w_passport_office_id integer not null,
+    w_post_index varchar(10),
+    w_street_code integer not null,
+    w_building varchar(10) not null,
+    w_extension varchar(10),
+    w_apartment varchar(10),
+    certificate_id varchar(20) not null,
+    register_office_id integer not null,
+    marriage_date date not null,
+    PRIMARY KEY (student_order_id),
+    FOREIGN KEY (h_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (w_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
 );
 
-create table jc_student_child
+CREATE TABLE jc_student_child
 (
-    student_child_id serial,
-    jc_student_order_id integer not null,
+    student_child_id SERIAL,
+    student_order_id integer not null,
     c_sur_name varchar(100) not null,
     c_given_name varchar(100) not null,
     c_patronymic varchar(100) not null,
-    c_certificate_number varchar (20) not null,
-    c_issue_date date not null,
+    c_date_of_birth date not null,
+    c_certificate_number varchar(10) not null,
+    c_certificate_date date not null,
     c_register_office_id integer not null,
     c_post_index varchar(10),
     c_street_code integer not null,
     c_building varchar(10) not null,
     c_extension varchar(10),
     c_apartment varchar(10),
-
-    primary key (student_child_id),
-    foreign key (c_street_code) references jc_street(street_code) on delete restrict,
-    foreign key (c_register_office_id) references jc_register_office(r_office_id) on delete restrict
-)
+    PRIMARY KEY (student_child_id),
+    FOREIGN KEY (c_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (c_register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
+);
